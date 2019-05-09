72ba28e05234bb29c02e53329d33e77d5685b2b3
yoseplee <joseph38.local@gmail.com>
2019-05-09 19:50:49 +0900

    modify scss files to apply aside-content layout. need more improvements

--- 1_front/klein-schwarze-box/src/views/security.vue
+++ 1_front/klein-schwarze-box/src/views/security.vue
@@ -1,21 +1,76 @@
 <template>
-  <section class="consensus create">
+  <section class="content">
+    <aside class="left-guide">
+      <ul class="fields">
+        <li>
+          <a href="#">
+            <h4>1. Hash</h4>
+          </a>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sem ex, maximus at consectetur a, ornare ut 
+mauris. Nam iaculis aliquam sem. Vivamus id lacus maximus, semper purus posuere, sagittis tellus. Morbi 
+dapibus a ex eu ornare. Aliquam vestibulum dapibus purus scelerisque tincidunt. Ut lobortis, ligula sit amet 
+ornare dapibus, enim purus venenatis urna, at laoreet nisl turpis a dui. Vestibulum imperdiet augue quis 
+vestibulum tincidunt.</p>
+        </li>
+        <li>
+          <a href="#">
+            <h4>2. Asymmetric Cryptography</h4>
+          </a>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sem ex, maximus at consectetur a, ornare ut 
+mauris. Nam iaculis aliquam sem. Vivamus id lacus maximus, semper purus posuere, sagittis tellus. Morbi 
+dapibus a ex eu ornare. Aliquam vestibulum dapibus purus scelerisque tincidunt. Ut lobortis, ligula sit amet 
+ornare dapibus, enim purus venenatis urna, at laoreet nisl turpis a dui. Vestibulum imperdiet augue quis 
+vestibulum tincidunt.</p>
+        </li>
+        <li>
+          <a href="#">
+            <h4>3. Digital Signature</h4>
+          </a>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sem ex, maximus at consectetur a, ornare ut 
+mauris. Nam iaculis aliquam sem. Vivamus id lacus maximus, semper purus posuere, sagittis tellus. Morbi 
+dapibus a ex eu ornare. Aliquam vestibulum dapibus purus scelerisque tincidunt. Ut lobortis, ligula sit amet 
+ornare dapibus, enim purus venenatis urna, at laoreet nisl turpis a dui. Vestibulum imperdiet augue quis 
+vestibulum tincidunt.</p>
+        </li>
+      </ul>
+    </aside>
+    <div class="program">
       <form action="" method="post" @submit.prevent="test">
-        <fieldset><legend>DEV PAGE</legend>
-        <ul class="fields">
+        <fieldset><legend>SECURITY</legend>
+          <ul class="fields">
             <li>
-              <label class="input-label">
-                  <span class="pre"><i class="fas fa-pen"></i></span>
-                  <input type="text" class="full-width" ref="title" v-model="testInput" size="80" required />
-                  <span class="lbl">값을 입력하세요</span>
-              </label>
+              <h4>Data</h4>
             </li>
             <li>
-              <button type="submit" class="btn full submit" @click.prevent="testSubmit">테스트확인 {{text}}</button>
+              <label class="input-label linear">
+                  <div class="coll left">
+                    <span class="pre"><i class="fas fa-pen"></i></span>
+                    <input type="text" class="full-width" ref="title" v-model="testInput" size="80" required />
+                    <span class="lbl">값을 입력하세요</span>
+                  </div>
+                  <div class="coll right">
+                    <button type="submit" class="btn full submit content-format" @click.prevent="testSubmit">Generate Hash {{text}}</button>
+                  </div>
+              </label>
+            </li>
+          </ul>
+          <ul class="fields">
+            <li>
+              <h4>Hash</h4>
+            </li>
+            <li> 
+              <label class="input-label linear">
+                <div class="coll">
+                  <span class="pre"><i class="fas fa-pen"></i></span>
+                  <input type="text" class="full-width" ref="title" v-model="testInput" size="80" required />
+                  <span class="lbl">Hash된 결과</span>
+                </div>
+              </label>
             </li>
         </ul>
         </fieldset>
       </form>
+    </div>
   </section>
 </template>
 
