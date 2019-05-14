8d9558d8cb2d7a5ff54c689f951991fbc41b0d3d
aiia <aiia@aiiaui-MacBookAir.local>
2019-05-14 15:04:00 +0900

    add hash code from java legacy code.

--- 2_backend/KleinSchwarzeBox/src/main/java/info/dkuswai/abc/KleinSchwarzeBox/controller/ApiController.java
+++ 2_backend/KleinSchwarzeBox/src/main/java/info/dkuswai/abc/KleinSchwarzeBox/controller/ApiController.java
@@ -1,13 +1,12 @@
 package info.dkuswai.abc.KleinSchwarzeBox.controller;
 
 import java.util.HashMap;
-
 import javax.annotation.Resource;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import info.dkuswai.abc.KleinSchwarzeBox.mapper.BasicMapper;
+import info.dkuswai.abc.KleinSchwarzeBox.core.security.*;
 
 @RestController
 public class ApiController {
@@ -21,4 +20,31 @@ public class ApiController {
         obj.put("nema", "lee");
         return obj;
     }
+
+    @GetMapping(value = "/api/hash")
+    public HashMap<String, Object> apiHash() {
+        HashMap<String, Object> obj = new HashMap<String, Object>();
+        String data = "11";
+        obj.put("success", true);
+        
+        //If there is an error when data hashed by function named generatHashfunction(),
+        //catch an error using exception. 
+        //And change the value of key named "success". 
+        try {
+            obj.put("data", generatHashfunction(data));
+        } catch(Exception exception) {
+            obj.put("success", false);
+            exception.printStackTrace();
+        }
+
+        return obj;
+    }
+
+    private String generatHashfunction(String data){
+    	Hash hashfun = new Hash("MD5");
+    	byte[] hash = hashfun.hashFn(data);
+		data= hashfun.bytesToString(hash);
+		return data;
+    }
+
 }
