1de452ca6ca0b36291e8a427f71c94d99d245fa2
JEONGCHAEEUN <ddj03340@naver.com>
2019-05-21 18:38:02 +0900

    add generateKey function

--- 2_backend/KleinSchwarzeBox/src/main/java/info/dkuswai/abc/KleinSchwarzeBox/controller/ApiController.java
+++ 2_backend/KleinSchwarzeBox/src/main/java/info/dkuswai/abc/KleinSchwarzeBox/controller/ApiController.java
@@ -1,5 +1,6 @@
 package info.dkuswai.abc.KleinSchwarzeBox.controller;
 
+import java.security.PublicKey;
 import java.util.HashMap;
 import javax.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +15,14 @@ import info.dkuswai.abc.KleinSchwarzeBox.core.security.*;
 public class ApiController {
     @Resource
     BasicMapper basicMapper;
+    private KeyService key ;
+
 
     @GetMapping(value = "/api")
     public HashMap<String, Object> apiHome() {
         HashMap<String, Object> obj = new HashMap<String, Object>();
         obj.put("success", true);
-        obj.put("nema", "lee");
+        obj.put("name", "lee");
         return obj;
     }
 
@@ -31,7 +34,7 @@ public class ApiController {
         //get request parameter from application
         //and take "data" key to digest
         System.out.println(params);
-        String data = params.get("data").toString();
+        String data = params.get("sdf").toString();
         obj.put("success", true);
         
         //If there is an error when data hashed by function named generatHashfunction(),
@@ -46,12 +49,100 @@ public class ApiController {
 
         return obj;
     }
-
     private String generatHashfunction(String data){
     	Hash hashfun = new Hash("MD5");
     	byte[] hash = hashfun.hashFn(data);
 		data= hashfun.bytesToString(hash);
 		return data;
     }
+
+    /*Assymetric Cryptography from here*/
+    @GetMapping(value = "/api/genkey")
+    public HashMap<String, Object> apiGenerateKey(){
+        HashMap<String, Object> obj = new HashMap<String, Object>();
+        //If there is an error ,
+        //catch an error using exception. 
+        //And change the value of key named "success". 
+        try {
+            obj.put("success", true);
+            key = new KeyService();
+            Hash hashfun = new Hash("MD5");
+            //generate public key
+            String pubLabel = hashfun.bytesToString(key.getPublicKey().getEncoded());
+            StringBuffer pubBuffer = new StringBuffer(pubLabel);
+            for(int i =0; i<pubBuffer.length(); i++){
+                if(i%130 == 0){
+                    pubBuffer.insert(i, "\n");
+                }
+            }
+            pubLabel = pubBuffer.toString();
+            
+            //generate private key
+            String priLabel = hashfun.bytesToString(key.getPrivateKey().getEncoded());
+            StringBuffer priBuffer = new StringBuffer(priLabel);
+            for(int i =0; i<priBuffer.length(); i++){
+                if(i%130== 0){
+                    priBuffer.insert(i, "\n");
+                }
+            }
+            priLabel = priBuffer.toString();
+            
+            //print public key and private key into json type
+            obj.put("PublicKey: ", pubLabel);
+            obj.put("PrivateKey: ", priLabel);
+            
+        } catch(Exception exception) {
+                obj.put("success", false);
+                exception.printStackTrace();
+        }
+        return obj;
+    }
+
+    @GetMapping(value = "/api/puben")
+    private HashMap<String, Object> publicEncryption(@RequestParam HashMap<String, Object> params){
+        HashMap<String, Object> obj = new HashMap<String, Object>();
+        String hashData = params.get("data").toString();
+        PublicKey pubkey = (PublicKey)params.get("pubkey");
+        key = new KeyService();
+
+        System.out.println(hashData);
+        String publicKey = key.encryptTextToPublic(hashData);
+
+        try{
+            
+            obj.put("publicKey", publicKey);
+        } catch(Exception e){
+            obj.put("success", false);
+            e.printStackTrace();
+            //System.out.println("<script>alert('Encryption Error!'); history.go(-1);</script>");
+        }
+        return obj;
+    }
+
+//     @RequestMapping(value = "/api/pd")
+//     private HashMap<String, Object> publicDecryption(@RequestBody HashMap<String, Object> param){
+//         HashMap<String, Object> obj = new HashMap<String, Object>();
+//         try{
+//             String privateKey = (String) param.get("publicKey");
+//             obj.put("hashData", key.decryptTextToPublic(privateKey));
+//             obj.put("success", true);
+//         }
+//         catch(Exception e){
+//             obj.put("success", false);
+//             e.printStackTrace();
+//             System.out.println("<script>alert('Decryption Error!'); history.go(-1);</script>");
+//         }
+//         return obj;
+//     }
+    /*private void encryptAlert(){
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Encryption Error!");
+        alert.setHeaderText(null);
+        alert.setContentText("Generate key first!");
+
+        alert.showAndWait();
+    }*/
+
+
 
 }
