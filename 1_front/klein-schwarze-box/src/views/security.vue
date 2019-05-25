15a0f1060948f4610ac4bcb019f18de88ba0c9dd
yoseplee <joseph38.local@gmail.com>
2019-05-25 21:59:46 +0900

    add frontend html and vue code of digital signature. API implementation and test will be followed

--- 1_front/klein-schwarze-box/src/views/security.vue
+++ 1_front/klein-schwarze-box/src/views/security.vue
@@ -245,7 +245,135 @@
             </fieldset>
           </div>
           <div v-show="step === 3">
-            <h4>Sorry! We are work in progress</h4>
+            <fieldset>
+              <legend>Asymmetric Key Generation</legend>
+              <ul class="fields">
+                <li>
+                  <h4>Public Key</h4>
+                </li>
+                <li>
+                  <label class="input-label linear">
+                    <div class="coll">
+                      <span class="pre">
+                        <i class="fas fa-pen"></i>
+                      </span>
+                      <input type="text" class="full-width" ref="title" v-model="publicKey" size="80" required>
+                    </div>
+                  </label>
+                </li>
+              </ul>
+              <ul class="fields">
+                <li>
+                  <h4>Private Key</h4>
+                </li>
+                <li>
+                  <label class="input-label linear">
+                    <div class="coll">
+                      <span class="pre">
+                        <i class="fas fa-pen"></i>
+                      </span>
+                      <input type="text" class="full-width" ref="title" v-model="privateKey" size="80" required>
+                    </div>
+                  </label>
+                </li>
+              </ul>
+            </fieldset>
+            <fieldset>
+              <legend>DITIGAL SIGNATURE</legend>
+              <ul class="fields">
+                <li>
+                  <h4>Sender</h4>
+                </li>
+                <li>
+                  <label class="input-label linear">
+                    <div class="coll left">
+                      <span class="pre">
+                        <i class="fas fa-pen"></i>
+                      </span>
+                      <input type="text" class="full-width" ref="title" v-model="plainText" size="80" required>
+                      <span class="lbl">상대방에게 전송할 데이터를 입력하세요</span>
+                    </div>
+                  </label>
+                </li>
+                <li>
+                  <label class="input-label linear">
+                    <div class="coll left">
+                      <span class="pre">
+                        <i class="fas fa-pen"></i>
+                      </span>
+                      <input type="text" class="full-width" ref="title" v-model="Hashed" size="80" required>
+                      <span class="lbl">전송할 데이터의 Hash값입니다</span>
+                    </div>
+                    <div class="coll right">
+                      <button type="submit" class="btn full submit content-format" @click.prevent="">Generate Signature</button>
+                    </div>
+                  </label>
+                </li>
+                <li>
+                  <label class="input-label linear">
+                    <div class="coll">
+                      <span class="pre">
+                        <i class="fas fa-pen"></i>
+                      </span>
+                      <input type="text" class="full-width" ref="title" v-model="cipherText" size="80" required>
+                      <span class="lbl">생성된 Digital Signature</span>
+                    </div>
+                  </label>
+                </li>
+              </ul>
+              <ul class="fields">
+                <li>
+                  <h4>Receiver</h4>
+                </li>
+                <li>
+                  <label class="input-label linear">
+                    <div class="coll">
+                      <span class="pre">
+                        <i class="fas fa-pen"></i>
+                      </span>
+                      <input type="text" class="full-width" ref="title" v-model="plainText" size="80" required>
+                      <span class="lbl">Received Data</span>
+                    </div>                    
+                  </label>
+                </li>
+                <li>
+                  <label class="input-label linear">
+                    <div class="coll">
+                      <span class="pre">
+                        <i class="fas fa-pen"></i>
+                      </span>
+                      <input type="text" class="full-width" ref="title" v-model="hashed" size="80" required>
+                      <span class="lbl">Hash of received data</span>
+                    </div>                    
+                  </label>
+                </li>
+                <li>
+                  <label class="input-label linear">
+                    <div class="coll">
+                      <span class="pre">
+                        <i class="fas fa-pen"></i>
+                      </span>
+                      <input type="text" class="full-width" ref="title" v-model="cipherText" size="80" required>
+                      <span class="lbl">Received Digital Signature</span>
+                    </div>
+                    <div class="coll right">
+                      <button type="submit" class="btn full submit content-format" @click.prevent="cryptoControl('decrypt')">Check Signature</button>
+                    </div>
+                  </label>
+                </li>
+                <li>
+                  <label class="input-label linear">
+                    <div class="coll">
+                      <span class="pre">
+                        <i class="fas fa-pen"></i>
+                      </span>
+                      <input type="text" class="full-width" ref="title" v-model="decrypted" size="80" required>
+                      <span class="lbl">Decrypted Digital Signature</span>
+                    </div>
+                  </label>
+                </li>
+              </ul>
+            </fieldset>
           </div>
         </div>
       </form>
@@ -261,7 +389,7 @@ export default {
     return {
       text: "",
       toHash: "",
-      step: 2,
+      step: 3,
       hashed: "",
       privateKey: "",
       publicKey: "",
@@ -337,7 +465,7 @@ export default {
       }
     },
     goToStep(num) {
-      if(num == 2 ) this.getKeyPair()
+      if(num == 2 | num == 3) this.getKeyPair()
       this.step = num;
     }
   }
