6090b5dbc63a2afeff1d255ee08218e0b2ccf23c
yoseplee <joseph38.local@gmail.com>
2019-05-29 20:39:51 +0900

    add test code for key generation part

--- 2_backend/KleinSchwarzeBox/src/test/java/info/dkuswai/abc/KleinSchwarzeBox/KleinSchwarzeBoxApplicationTests.java
+++ 2_backend/KleinSchwarzeBox/src/test/java/info/dkuswai/abc/KleinSchwarzeBox/KleinSchwarzeBoxApplicationTests.java
@@ -12,12 +12,22 @@ public class KleinSchwarzeBoxApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		tinyBlockInitTest();
+	}
+
+	public void keyTest() {
+		//test for generating asymmetric key cryptography
+		//espcially for testing via encoding and decoding using base64 of java.util
 		KeyService keyService = new KeyService();
 		System.out.println("key test");
 		System.out.println(keyService.getPublicKey().getEncoded());
 		keyService.storeKey("abcde");
 		keyService.loadKey("abcde");
 		System.out.println(keyService.getPublicKey().getEncoded().toString());
+	}
+
+	public void tinyBlockInitTest() {
+
 	}
 
 }
